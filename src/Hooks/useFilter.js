553d5d2e7119aec8dd91ee5b6ffb352974ddaa44
vishal1122-ai <60536075+vishal1122-ai@users.noreply.github.com>
2024-09-25 14:26:09 -0400

    Add remaining project files

---
+++ src/Hooks/useFilter.js
@@ -0,0 +1,11 @@
+import { useLocalStorage } from "./useLocalStorage";
+
+export function useFilter(dataList, callback) {
+  const [query, setQuery] = useLocalStorage("query", "");
+
+  const filteredData = dataList.filter((data) =>
+    callback(data).toLowerCase().includes(query)
+  );
+
+  return [filteredData, setQuery];
+}
